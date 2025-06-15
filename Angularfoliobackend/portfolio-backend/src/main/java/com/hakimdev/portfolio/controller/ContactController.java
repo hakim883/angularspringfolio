f4d623989ab74dbed8c061110732c253291ff2ba
admin <admin@MacBook-Pro-de-admin.local>
2025-06-15 18:49:27 +0200

    Ajout service greeting et liaison backend

--- Angularfoliobackend/portfolio-backend/src/main/java/com/hakimdev/portfolio/controller/ContactController.java
+++ Angularfoliobackend/portfolio-backend/src/main/java/com/hakimdev/portfolio/controller/ContactController.java
@@ -1,5 +1,16 @@
 package com.hakimdev.portfolio.controller;
 
+import com.hakimdev.portfolio.entity.ContactMessage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api")
 public class ContactController {
 
+    @PostMapping("/contact")
+    public ResponseEntity<String> handleContact(@RequestBody ContactMessage message) {
+        // Ici tu peux traiter le message, l'enregistrer, envoyer un email, etc.
+        System.out.println("Message reçu: " + message);
+        return ResponseEntity.ok("Message reçu avec succès");
+    }
 }
