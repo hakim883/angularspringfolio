81543f95680edbdc7374345cc51bf11acd0415ad
admin <admin@MacBook-Pro-de-admin.local>
2025-06-18 00:53:18 +0200

    Description

--- Angularfoliobackend/portfolio-backend/src/main/java/com/hakimdev/portfolio/WebConfig.java
+++ Angularfoliobackend/portfolio-backend/src/main/java/com/hakimdev/portfolio/WebConfig.java
@@ -6,17 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Applique à tous les endpoints sous /api
+                .allowedOrigins("http://localhost:4200") // L'URL de ton front-end Angular
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
